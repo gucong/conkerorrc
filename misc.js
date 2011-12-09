3aab5090ccc92532853c862cf11b3e92216ec459
gucong <gucong43216@gmail.com>
2011-12-08 22:15:14 -0600

    modified:   misc.js 	modified:   stylesheets/my-theme/theme.json 	modified:   webjumps.js

--- misc.js
+++ misc.js
@@ -4,6 +4,9 @@ editor_shell_command = "$EDITOR";
 // view source in your editor.
 view_source_use_external_editor = true;
 
+// don't kill last buffer.
+can_kill_last_buffer = false;
+
 // show buffer count
 remove_hook("mode_line_hook", mode_line_adder(clock_widget));
 add_hook("mode_line_hook", mode_line_adder(loading_count_widget), true);
@@ -13,10 +16,12 @@ add_hook("mode_line_hook", mode_line_adder(buffer_count_widget), true);
 key_bindings_ignore_capslock = true;
 define_key(content_buffer_normal_keymap, "d", "follow-new-buffer-background");
 define_key(content_buffer_normal_keymap, "delete", "delete");
+define_key(content_buffer_normal_keymap, "* f", "browser-object-file");
 
 // extension compatibility
 user_pref("extensions.checkCompatibility", false);
 user_pref("extensions.checkUpdateSecurity", false);
+user_pref("general.useragent.compatMode.firefox", true);
 
 // page modes
 require("page-modes/google-search-results.js");
@@ -26,13 +31,13 @@ require("page-modes/google-maps.js");
 // session management
 require("session.js");
 
-// user agent
-default_pref("general.useragent.extra.firefox", "firefox/5.0");
-
 // default completion
 url_completion_use_bookmarks = false;
 url_completion_use_history = false;
 url_completion_use_webjumps = true;
+
+// download target
+download_buffer_automatic_open_target = OPEN_NEW_BUFFER;
 
 // test
 // define_key(content_buffer_normal_keymap, "C-w",
--- webjumps.js
+++ webjumps.js
@@ -15,10 +15,13 @@
 // webjumps
 define_webjump("reader", "https://reader.google.com");
 define_webjump("mldonkey", "http://localhost:4080");
-define_webjump("aur", "http://aur.archlinux.org/packages.php?K=%s");
+define_webjump("aur", "https://aur.archlinux.org/packages.php?K=%s");
 define_webjump("archwiki", "http://wiki.archlinux.org/index.php?search=%s");
 define_webjump("taobao", "http://s.taobao.com/search?q=%s");
 define_webjump("archlinux", "http://www.archlinux.org/packages/?q=%s");
 define_webjump("ip", "http://www.123cha.com/ip/?q=%s");
 define_webjump("simplecd", "http://www.simplecd.org/?q=%s");
 define_webjump("hatrix", "http://hatrix.org/files/%s");
+define_webjump("math607", "http://www.math.tamu.edu/~manshel/m607/m607.html");
+define_webjump("math653", "http://www.math.tamu.edu/~stiller/courses/math653.html");
+define_webjump("math641", "http://www.math.tamu.edu/~francis.narcowich/m641/f11/m641f11_hw.html");
